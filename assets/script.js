f79f4296b442cb772cafe8e7df3dec30306216be
rnahill <rei333lly@gmail.com>
2023-10-27 13:40:09 -0400

    finished save function

--- assets/script.js
+++ assets/script.js
@@ -57,6 +57,7 @@ function createCalendar () {
 
     calendarContainer.append(timeBlock);
 
+
     }
 
 }
@@ -64,25 +65,47 @@ function createCalendar () {
 // Run createCalendar function so calendar appears on the page.
 createCalendar();
 
-
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+ // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  $(function () {
   
   var timeBtnEl = $('.saveBtn');
 
   timeBtnEl.on("click", function(){
     
-    var savedData = $('.description').val();
-    localStorage.setItem("savedData", savedData);
-    alert("Data saved");
+    var chosenTimeBlock = $(this).parent();
+    
+    var timeBlockId = chosenTimeBlock.attr('Hour-' + i);
 
-  })
+    var textToStore = chosenTimeBlock.find('.description').val();
 
+    var dataToStore = {
+      text: textToStore
+    };
+
+    localStorage.setItem(timeBlockId, JSON.stringify(dataToStore));
+    
+  )}
+
+
+  //   if (localStorage.getItem(timeBlockId) !== null) {
+  //     // Data is stored in localStorage with the key 'yourKey'
+  //     // You can access and use it here
+  //     var storedData = localStorage.getItem(timeBlockId);
+  //     console.log('Data is stored: ' + storedData);
+  //   } else {
+  //     // Data is not stored in localStorage with the key 'yourKey'
+  //     console.log('No data stored with the key timeBlockID');
+  //   }
+  // })
+
+
+ 
 
 
 
